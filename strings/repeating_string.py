4a5fa4dd59ae1c98ef7bfa219af5b33ad8be100c
Mehul Arora <60281183+sociallyencrypted@users.noreply.github.com>
2021-01-15 12:33:02 +0530

    Add files via upload
    This file takes a string and checks for repeating substrings in the file
    For Eg. "abcabcabc" gives "abc"

---
+++ strings/repeating_string.py
@@ -0,0 +1,11 @@
+#Made by Mehul Arora on 12 Jan 2021
+#Checks for a repeating sequence in a string
+#For eg. "abcabcabc" returns "abc"
+def repStr(string1: str) -> str:
+    tmp=string1*2
+    tmp=tmp[1:-1]
+    index=tmp.find(string1)
+    return string1[0:index]
+
+if __name__ == "__main__":
+    print(repStr(input("Please input sequence: ")))
